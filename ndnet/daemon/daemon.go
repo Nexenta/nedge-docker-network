eb320696ac573d293f462ab4e24d0e62f9708dd8
root <nabin.acharya@nexenta.com>
2016-09-15 15:26:29 -0700

    remove startup mesg

--- ndnet/daemon/daemon.go
+++ ndnet/daemon/daemon.go
@@ -21,6 +21,5 @@ func Start(cfgFile string, debug bool) {
 		log.Fatalf("ERROR: %s init failed!", PLUGIN_NAME)
 	}
 	h := dn.NewHandler(d)
-	log.Info("%s driver created & initialized", PLUGIN_NAME)
 	log.Info(h.ServeTCP(PLUGIN_NAME, PLUGIN_PORT))
 }
