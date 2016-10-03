b373b08275d804218fd21bd80ffc2989e39b0310
Qeas <alexey.khodos@nexenta.com>
2016-10-03 17:41:42 +0300

    run daemon in background with pid file

--- ndnet/ndnetcli/daemoncli.go
+++ ndnet/ndnetcli/daemoncli.go
@@ -2,7 +2,10 @@ package ndnetcli
 
 import (
 	"github.com/codegangsta/cli"
-	"github.com/Nexenta/nedge-docker-network/ndnet/daemon"
+	ndnetDaemon "github.com/Nexenta/nedge-docker-network/ndnet/daemon"
+	"github.com/sevlyar/go-daemon"
+	log "github.com/Sirupsen/logrus"
+	"syscall"
 )
 
 var (
@@ -11,6 +14,7 @@ var (
 		Usage: "daemon related commands",
 		Subcommands: []cli.Command{
 			DaemonStartCmd,
+			DaemonStopCmd,
 		},
 	}
 
@@ -29,13 +33,65 @@ var (
 		},
 		Action: cmdDaemonStart,
 	}
+	
+	DaemonStopCmd = cli.Command{
+		Name: "stop",
+		Usage: "Stop the Nedge Docker Daemon: `stop",
+		Action: cmdDaemonStop,
+	}
 )
 
+func cmdDaemonStop(c *cli.Context) {
+	cntxt := &daemon.Context{
+		PidFileName: "/opt/nedge/var/run/ndnet.pid",
+		PidFilePerm: 0644,
+		LogFileName: "/opt/nedge/var/log/ndnet.log",
+		LogFilePerm: 0640,
+		Umask:       027,
+	}
+	d, err := cntxt.Search()
+	if err != nil {
+		log.Fatalln("Unable to send signal to the daemon:", err)
+	}
+	d.Signal(syscall.SIGTERM)
+}
+
 func cmdDaemonStart(c *cli.Context) {
+	log.Println("CMD DAEMON START")
+	cntxt := &daemon.Context{
+		PidFileName: "/opt/nedge/var/run/ndnet.pid",
+		PidFilePerm: 0644,
+		LogFileName: "/opt/nedge/var/log/ndnet.log",
+		LogFilePerm: 0640,
+		Umask:       027,
+	}
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer cntxt.Release()
+	if d != nil {
+		log.Println("RETURN")
+		return
+	}
+
+	log.Info("- - - - - - - - - - - - - - -")
+	log.Info("Daemon started")
+	go DaemonStart(c)
+
+	err = daemon.ServeSignals()
+	if err != nil {
+		log.Info("Error:", err)
+	}
+	log.Info("Daemon terminated")
+}
+
+func DaemonStart(c *cli.Context) {
+	log.Println("DAEMON START")
 	verbose := c.Bool("verbose")
 	cfg := c.String("config")
 	if cfg == "" {
 		cfg = "/opt/nedge/etc/ccow/ndnet.json"
 	}
-	daemon.Start(cfg, verbose)
+	ndnetDaemon.Start(cfg, verbose)
 }
